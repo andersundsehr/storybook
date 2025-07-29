1108f89832c50ea9de3ac9c05b97b510da3106a5
Matthias Vogel <m.vogel@andersundsehr.com>
2025-07-29 08:20:06 +0200

    📝 make it clear that this renders Fluid Components

--- ext_emconf.php
+++ ext_emconf.php
@@ -5,7 +5,7 @@ use Composer\InstalledVersions;
 /** @var string $_EXTKEY */
 $EM_CONF[$_EXTKEY] = [
     'title' => 'EXT:storybook',
-    'description' => 'The one and only Storybook Renderer for Fluid',
+    'description' => 'The one and only Storybook Renderer for TYPO3 Fluid Components',
     'category' => 'module',
     'author' => 'Matthias Vogel',
     'author_email' => 'm.vogel@andersundsehr.com',

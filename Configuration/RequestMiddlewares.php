ba5cbacf1544898e55f78ab7676955387dbe021a
Matthias Vogel <m.vogel@andersundsehr.com>
2026-04-20 09:47:51 +0200

    [TASK] fix pipeline

--- Configuration/RequestMiddlewares.php
+++ Configuration/RequestMiddlewares.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 use Andersundsehr\Storybook\Middleware\StorybookMiddleware;
 
 return [
--- Documentation/dummy-project/src/extensions/dummy_extension/ext_localconf.php
+++ Documentation/dummy-project/src/extensions/dummy_extension/ext_localconf.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 use Andersundsehr\DummyExtension\ComponentCollection;
 
 # This is required so EXT:storybook can find the component collection
